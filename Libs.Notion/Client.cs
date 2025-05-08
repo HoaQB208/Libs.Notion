e0a6b0af17fb55581e390a66af0c287e5a36aeb6
Thái Hoà Phạm <hoaqb208@gmail.com>
2025-05-08 09:27:30 +0700

    Init project

---
+++ Libs.Notion/Client.cs
@@ -0,0 +1,21 @@
+﻿using Notion.Client;
+
+namespace Libs.Notion
+{
+    public class Client
+    {
+        /// <summary>
+        /// Get Notion Client
+        /// </summary>
+        /// <param name="token">https://www.notion.so/my-integrations</param>
+        /// Chia sẻ page với Client API này: Dấu 3 chấm (trên, phải) >> Connections >> Chọn Client API đã tạo
+        /// <returns></returns>
+        public static NotionClient Get(string token)
+        {
+            return NotionClientFactory.Create(new ClientOptions
+            {
+                AuthToken = token
+            });
+        }
+    }
+}
---
+++ Libs.Notion/Database.cs
@@ -0,0 +1,76 @@
+﻿using Notion.Client;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Libs.Notion
+{
+    public class Database
+    {
+        /// <summary>
+        /// Database functs
+        /// </summary>
+        /// <param name="client"></param>
+        /// <param name="databaseId">Chuỗi 32 ký tự sau tên miền và trước tham số '?v='
+        /// và cần chia sẻ database với Client API đã tạo: Dấu 3 chấm (trên, phải) >> Connections >> Chọn Client API đã tạo</param>
+        /// <returns></returns>
+        public static async Task<List<Page>> Get(NotionClient client, string databaseId)
+        {
+            var rows = new List<Page>();
+
+            try
+            {
+                var queryParams = new DatabasesQueryParameters();
+                var queryResponse = await client.Databases.QueryAsync(databaseId, queryParams);
+
+                rows.AddRange(queryResponse.Results);
+
+                while (queryResponse.HasMore)
+                {
+                    queryParams.StartCursor = queryResponse.NextCursor;
+                    queryResponse = await client.Databases.QueryAsync(databaseId, queryParams);
+                    rows.AddRange(queryResponse.Results);
+                }
+            }
+            catch (NotionApiException apiEx)
+            {
+                Console.WriteLine($"API Error: {apiEx.StatusCode} - {apiEx.Message}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"General Error: {ex.Message}");
+            }
+
+            return rows;
+        }
+
+        public static async Task Update(NotionClient client, string databaseId, string rowId, string newContent)
+        {
+            // Tạo bản cập nhật cho cột "Content"
+            var updatedProperties = new Dictionary<string, PropertyValue>
+            {
+                {
+                    "Content", new RichTextPropertyValue
+                    {
+                        RichText = new List<RichTextBase>
+                        {
+                            new RichTextText
+                            {
+                                Text = new Text
+                                {
+                                    Content = newContent
+                                }
+                            }
+                        }
+                    }
+                }
+            };
+
+            // Cập nhật hàng đầu tiên
+            await client.Pages.UpdateAsync(rowId, new PagesUpdateParameters
+            {
+                Properties = updatedProperties
+            });
+        }
+    }
+}
